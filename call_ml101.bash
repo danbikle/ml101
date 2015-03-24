d9325e32e703198fae16204a7348576775032b12
Dan Bikle <dan.bikle@gmail.com>
2015-03-24 05:13:02 +0000

    2nd-script

--- call_ml101.bash
+++ call_ml101.bash
@@ -4,9 +4,17 @@
 
 # This script should call ml101.bash with correct variables.
 
-export YRS=2
-export TRAIN_YRS=4
-export HTML=YES
+# This should give me a YRS amount of predictions:
+export YRS=1
+
+# This should train from TRAIN_YRS amount of observations:
+export TRAIN_YRS=5
+
+# This should be yes when I want a prediction near calif-noon:55 m-f:
+export HTML=no
+# At night, if I set HTML=yes a duplicate observation of the most recent close
+# will appear in the data.
+# So, at night, export HTML=no
 
 cd ~ann/ml101/
 ./ml101.bash 
