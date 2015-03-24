fefbc1a5ef052c419240b924e6a4b6d54b0c9f02
Dan Bikle <dan.bikle@gmail.com>
2015-03-24 04:44:54 +0000

    2nd-script

---
+++ ml101.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# /home/ann/ml101/ml101.bash
+
+# This script should learn from GSPC observations and then predict.
+
+mkdir -p /tmp/ml101/
+cd       /tmp/ml101/
+
+echo  PCOUNT IS:
+echo $PCOUNT
+
+exit
