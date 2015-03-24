683c5b1c04c2523c3827ef6857616a94bdbed90b
Dan Bikle <dan.bikle@gmail.com>
2015-03-24 05:07:08 +0000

    2nd-script

--- call_ml101.bash
+++ call_ml101.bash
@@ -4,6 +4,9 @@
 
 # This script should call ml101.bash with correct variables.
 
-export PCOUNT=252
+export YRS=2
+export TRAIN_YRS=4
+export HTML=YES
+
 cd ~ann/ml101/
 ./ml101.bash 
--- ml101.bash
+++ ml101.bash
@@ -4,10 +4,38 @@
 
 # This script should learn from GSPC observations and then predict.
 
+if [ -e ~ann/gspc/ ]; then
+  echo $0 is in the correct folder.
+else
+  echo $0 needs to reside here:
+  echo ~ann/gspc/
+  echo bye.
+  exit 1
+fi
+
 mkdir -p /tmp/ml101/
 cd       /tmp/ml101/
 
-echo  PCOUNT IS:
-echo $PCOUNT
+
+if [ -z "$YRS" ]; then
+    export YRS=2
+fi  
+
+if [ -z "$TRAIN_YRS" ]; then
+    export TRAIN_YRS=4
+fi  
+
+if [ -z "$HTML" ]; then
+    export HTML=no
+fi  
+
+echo  YRS IS:
+echo $YRS
+
+echo  TRAIN_YRS IS:
+echo $TRAIN_YRS
+
+echo  HTML IS:
+echo $HTML
 
 exit
